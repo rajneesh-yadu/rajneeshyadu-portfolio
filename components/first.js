f105f4e764a61197af409a726b00f42ed046ebb0
Rajneesh Yadu <rajneeshyadu23@gmail.com>
2022-02-28 10:12:43 +0530

    font changes

--- components/first.js
+++ components/first.js
@@ -3,9 +3,9 @@ export default function First() {
     <section id='intro'>
       <div className='flex flex-col justify-center xl:justify-center items-center h-screen rounded-br-full bg-gradient-to-tl from-canvas1-light via-canvas1-medium to-canvas1-dark'>
         <div>
-          <div className='text-gray-400 text-6xl  ml-6  xl:text-6xl bg-clip-text text-transparent bg-gradient-to-b  from-primary1-light via-primary1-medium to-primary1-dark'>
+          <div className='text-gray-400 text-2xl  ml-6  xl:text-4xl bg-clip-text text-transparent bg-gradient-to-b  from-primary1-light via-primary1-medium to-primary1-dark'>
             Hi, I am{' '}
-            <span className='text-secondary1-light animate-pulse font-extrabold text-6xl'>
+            <span className='text-secondary1-light animate-pulse font-extrabold text-4xl xl:text-6xl'>
               Rajneesh
             </span>
             {/* <span className='text-gray-500'>from </span>
@@ -13,8 +13,8 @@ export default function First() {
               India
             </span> */}
           </div>
-          <div className='text-gray-400 font-thin  ml-6 text-4xl xl:text-3xl bg-clip-text text-transparent bg-gradient-to-t from-primary1-light via-primary1-medium to-primary1-dark'>
-            A Passionate Web developer
+          <div className='text-gray-400 font-thin  ml-6 text-2xl xl:text-4xl bg-clip-text text-transparent bg-gradient-to-t from-primary1-light via-primary1-medium to-primary1-dark'>
+            A Passionate Full Stack Web developer
           </div>
         </div>
         {/* <img src='/triangle1.svg' className='bg-triangle1 h-3/4 w-screen'></img> */}
