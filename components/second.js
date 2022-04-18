9cf0544b7b380b7400a136f3c5147b471e0a318e
Rajneesh Yadu <rajneeshyadu23@gmail.com>
2022-04-18 16:53:50 +0530

    updated project section

--- components/second.js
+++ components/second.js
@@ -14,18 +14,18 @@ export default function Second() {
       image: '/apod.jpg',
       url: 'https://ry-apod.vercel.app/',
     },
-    {
-      title: 'Saloon Shop',
-      price: ['JavaScript', 'ReactJs', 'NEXT JS', 'Tailwind CSS'],
-      image: '/saloon.jpg',
-      url: 'https://surajsaloon.vercel.app/',
-    },
+    // {
+    //   title: 'Saloon Shop',
+    //   price: ['JavaScript', 'ReactJs', 'NEXT JS', 'Tailwind CSS'],
+    //   image: '/saloon.jpg',
+    //   url: 'https://surajsaloon.vercel.app/',
+    // },
   ]
 
   return (
     <section id='projects' className='border border-transparent'>
       <div className='text-center text-secondary1-light text-3xl font-extrabold'>
-        Projects
+        Some Personal Projects
       </div>
       <div className='grid grid-cols-1 lg:grid-cols-2 justify-items-center place-content-center gap-5  lg:min-h-screen my-16 lg:my-8'>
         {cardData.map((card) => (
